fe7a4dbd9a96b98feec5a3c170f85c501079de62
Victor Queiroz <victorcqueirozg@gmail.com>
2018-04-25 21:55:14 -0300

    define `CustomTypeProcessor` as a ES6 class and export BinaryObject class

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const bo = require('bindings')('binobject');
 
-function CustomTypeProcessor() {
+class CustomTypeProcessor() {
 
 }
 
+class BinaryObject {
+    constructor(custom) {
+        this.custom = custom;
+    }
+    encode(object) {
+        return new bo.ObjectEncoder(this.custom).encode(object);
+    }
+    decode(buffer) {
+        return new bo.ObjectDecoder(buffer, this.custom).decode();
+    }
+}
+
+bo.BinaryObject = BinaryObject;
 bo.CustomTypeProcessor = CustomTypeProcessor;
 
 module.exports = bo;
