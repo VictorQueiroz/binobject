562c2c8909260e5f567ff57683657f8706b15415
Victor Queiroz <victorcqueirozg@gmail.com>
2018-04-25 21:20:31 -0300

    src/node-decoder.cc: create abstraction methods to handle decoder object

--- src/node-decoder.cc
+++ src/node-decoder.cc
@@ -1,5 +1,6 @@
 #include <nan.h>
 #include "decoder.h"
+#include "custom-type.h"
 #include "node-decoder.h"
 #include "node-binobject.h"
 
@@ -15,47 +16,79 @@ Decoder::~Decoder() {
     destroy_decoder(decoder);
 }
 
-Local<Value> ReadNumberAsValue(Isolate* isolate, bo_decoder* decoder, uint8_t type) {
+uint8_t Decoder::ReadUInt8() {
+    return read_uint8(decoder);
+}
+
+int8_t Decoder::ReadInt8() {
+    return read_int8(decoder);
+}
+
+uint16_t Decoder::ReadUInt16LE() {
+    return read_uint16_le(decoder);
+}
+
+int16_t Decoder::ReadInt16LE() {
+    return read_int16_le(decoder);
+}
+
+uint32_t Decoder::ReadUInt32LE() {
+    return read_uint32_le(decoder);
+}
+
+int32_t Decoder::ReadInt32LE() {
+    return read_int32_le(decoder);
+}
+
+double Decoder::ReadDoubleLE() {
+    return read_double_le(decoder);
+}
+
+void Decoder::ReadBytes(size_t length, uint8_t* buffer) {
+    read_bytes(decoder, length, buffer);
+}
+
+Local<Value> ReadNumberAsValue(Isolate* isolate, Decoder* decoder, uint8_t type) {
     switch(type) {
         case BO::UInt8:
-            return Number::New(isolate, read_uint8(decoder));
+            return Number::New(isolate, decoder->ReadUInt8());
         case BO::Int8:
-            return Number::New(isolate, read_int8(decoder));
+            return Number::New(isolate, decoder->ReadInt8());
         case BO::UInt16:
-            return Number::New(isolate, read_uint16_le(decoder));
+            return Number::New(isolate, decoder->ReadUInt16LE());
         case BO::Int16:
-            return Number::New(isolate, read_int16_le(decoder));
+            return Number::New(isolate, decoder->ReadInt16LE());
         case BO::UInt32:
-            return Number::New(isolate, read_uint32_le(decoder));
+            return Number::New(isolate, decoder->ReadUInt32LE());
         case BO::Int32:
-            return Number::New(isolate, read_int32_le(decoder));
+            return Number::New(isolate, decoder->ReadInt32LE());
     }
 
     return isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "Got invalid integer type")));
 }
 
-double ReadNumber(Isolate* isolate, bo_decoder* decoder) {
-    uint8_t type = read_uint8(decoder);
+double ReadNumber(Isolate* isolate, Decoder* decoder) {
+    uint8_t type = decoder->ReadUInt8();
     double n = 0;
 
     switch(type) {
         case BO::UInt8:
-            n += read_uint8(decoder);
+            n += decoder->ReadUInt8();
             break;
         case BO::Int8:
-            n += read_int8(decoder);
+            n += decoder->ReadInt8();
             break;
         case BO::UInt16:
-            n += read_uint16_le(decoder);
+            n += decoder->ReadUInt16LE();
             break;
         case BO::Int16:
-            n += read_int16_le(decoder);
+            n += decoder->ReadInt16LE();
             break;
         case BO::UInt32:
-            n += read_uint32_le(decoder);
+            n += decoder->ReadUInt32LE();
             break;
         case BO::Int32:
-            n += read_int32_le(decoder);
+            n += decoder->ReadInt32LE();
             break;
         default:
             isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "Got invalid integer type")));
@@ -64,14 +97,14 @@ double ReadNumber(Isolate* isolate, bo_decoder* decoder) {
     return n;
 }
 
-void ReadObject(Isolate* isolate, bo_decoder* decoder, Local<Object> result) {
+void ReadObject(Isolate* isolate, Decoder* decoder, Local<Object> result) {
     size_t properties_length = ReadNumber(isolate, decoder);
 
     for(size_t i = 0; i < properties_length; i++) {
         size_t string_length = ReadNumber(isolate, decoder);
         uint8_t* buffer = (uint8_t*)malloc(string_length);
 
-        read_bytes(decoder, string_length, buffer);
+        decoder->ReadBytes(string_length, buffer);
 
         Local<String> name = String::NewFromOneByte(isolate, buffer, NewStringType::kNormal, string_length).ToLocalChecked();
         Local<Value> value = ReadValue(isolate, decoder);
@@ -80,7 +113,7 @@ void ReadObject(Isolate* isolate, bo_decoder* decoder, Local<Object> result) {
     }
 }
 
-Local<Value> ReadArray(Isolate* isolate, bo_decoder* decoder) {
+Local<Value> ReadArray(Isolate* isolate, Decoder* decoder) {
     int array_length = ReadNumber(isolate, decoder);
     Local<Array> list = Array::New(isolate, array_length);
 
@@ -90,7 +123,7 @@ Local<Value> ReadArray(Isolate* isolate, bo_decoder* decoder) {
     return list;
 }
 
-Local<Value> ReadMapNative(Isolate* isolate, bo_decoder* decoder) {
+Local<Value> ReadMapNative(Isolate* isolate, Decoder* decoder) {
     Local<Map> map = Map::New(isolate);
     Local<Context> context = isolate->GetCallingContext();
     size_t map_length = ReadNumber(isolate, decoder);
@@ -103,20 +136,40 @@ Local<Value> ReadMapNative(Isolate* isolate, bo_decoder* decoder) {
     return map;
 }
 
-Local<Value> ReadValue(Isolate* isolate, bo_decoder* decoder) {
-    uint8_t type = read_uint8(decoder);
+bool CheckCustomType(Isolate* isolate, Decoder* decoder, uint8_t type, Local<Object>& processor) {
+    Local<Object> holder = decoder->GetCurrentHolder();
+    Local<Array> instructions = Local<Array>::Cast(holder->Get(String::NewFromUtf8(isolate, "instructions")));
+
+    uint32_t length = instructions->Length();
+
+    for(uint32_t i = 0; i < length; i++) {
+        Local<Object> instruction = instructions->Get(i)->ToObject();
+        Local<Uint32> value = Local<Uint32>::Cast(instruction->Get(String::NewFromUtf8(isolate, "value")));
+
+        if(value->Value() != type)
+            continue;
+
+        processor = instruction->Get(String::NewFromUtf8(isolate, "processor"))->ToObject();
+        return true;
+    }
+
+    return false;
+}
+
+Local<Value> ReadValue(Isolate* isolate, Decoder* decoder) {
+    uint8_t type = decoder->ReadUInt8();
 
     if(type == BO::Buffer) {
         size_t byte_length = ReadNumber(isolate, decoder);
         uint8_t* buffer = (uint8_t*) malloc(byte_length);
 
-        read_bytes(decoder, byte_length, buffer);
+        decoder->ReadBytes(byte_length, buffer);
 
         Local<Object> nodejs_buffer = node::Buffer::New(isolate, (char*) buffer, byte_length).ToLocalChecked();
 
         return nodejs_buffer;
     } else if(type == BO::Boolean) {
-        bool value = read_uint8(decoder) == 0 ? false : true;
+        bool value = decoder->ReadUInt8() == 0 ? false : true;
         Local<Boolean> boolean = Boolean::New(isolate, value);
 
         return boolean;
@@ -133,29 +186,51 @@ Local<Value> ReadValue(Isolate* isolate, bo_decoder* decoder) {
         int string_length = ReadNumber(isolate, decoder);
         uint8_t* buffer = (uint8_t*) malloc(string_length);
 
-        read_bytes(decoder, string_length, buffer);
+        decoder->ReadBytes(string_length, buffer);
 
         Local<String> string = String::NewFromOneByte(isolate, buffer, NewStringType::kNormal, string_length).ToLocalChecked();
 
         free(buffer);
         return string;
     } else if(type == BO::Date) {
-        double date = read_double_le(decoder);
+        double date = decoder->ReadDoubleLE();
 
         return Date::New(isolate->GetCallingContext(), date).ToLocalChecked();
     } else if(type == BO::Array) {
         return ReadArray(isolate, decoder);
     } else if(type == BO::Map) {
         return ReadMapNative(isolate, decoder);
+    } else {
+        Local<Object> processor;
+
+        if(CheckCustomType(isolate, decoder, type, processor)) {
+            size_t byte_length = ReadNumber(isolate, decoder);
+            uint8_t* buffer = (uint8_t*) malloc(byte_length);
+
+            decoder->ReadBytes(byte_length, buffer);
+
+            return CustomType::Decode(isolate, byte_length, buffer, processor);
+        }
     }
 
     return ReadNumberAsValue(isolate, decoder, type);
 }
 
+void Decoder::SetCurrentHolder(Local<Object> holder) {
+    current_holder = holder;
+}
+
+Local<Object> Decoder::GetCurrentHolder() {
+    return current_holder;
+}
+
 void Decoder::Decode(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
-    Decoder* wrapped_decoder = ObjectWrap::Unwrap<Decoder>(args.Holder());
-    Local<Value> result = ReadValue(isolate, wrapped_decoder->decoder);
+    Decoder* decoder = ObjectWrap::Unwrap<Decoder>(args.Holder());
+
+    decoder->SetCurrentHolder(args.Holder());
+
+    Local<Value> result = ReadValue(isolate, decoder);
 
     args.GetReturnValue().Set(result);
 }
@@ -175,6 +250,9 @@ void Decoder::CreateObject(const FunctionCallbackInfo<Value>& args) {
 
     size_t byte_length = node::Buffer::Length(value);
     uint8_t* buffer = (uint8_t*) node::Buffer::Data(value);
+
+    instance->Set(String::NewFromUtf8(isolate, "instructions"), args[1]);
+
     Decoder* decoder = new Decoder(byte_length, buffer);
     decoder->Wrap(instance);
 
--- src/node-decoder.h
+++ src/node-decoder.h
@@ -7,11 +7,10 @@
 
 using namespace v8;
 
-Local<Value> ReadValue(Isolate* isolate, bo_decoder* decoder);
-
 class Decoder : public node::ObjectWrap {
 private:
     bo_decoder* decoder;
+    Local<Object> current_holder;
     Decoder(size_t byte_length, uint8_t* buffer);
     ~Decoder();
     static Persistent<Function> constructor;
@@ -21,6 +20,19 @@ public:
     static void Init(Isolate* isolate);
     static void NewInstance(const FunctionCallbackInfo<Value>& args);
     static void CreateObject(const FunctionCallbackInfo<Value>& args);
+    void SetCurrentHolder(Local<Object> holder);
+    Local<Object> GetCurrentHolder();
+
+    uint8_t ReadUInt8();
+    int8_t ReadInt8();
+    uint16_t ReadUInt16LE();
+    int16_t ReadInt16LE();
+    uint32_t ReadUInt32LE();
+    int32_t ReadInt32LE();
+    double ReadDoubleLE();
+    void ReadBytes(size_t length, uint8_t* buffer);
 };
+
+Local<Value> ReadValue(Isolate* isolate, Decoder* decoder);
 
 #endif
