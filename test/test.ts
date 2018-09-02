518a77a289dc08c9e90446b0d3016225a579cd7f
Victor Queiroz <victorcqueirozg@gmail.com>
2018-09-02 00:53:25 -0300

    update to latest version of sarg

--- test/test.ts
+++ test/test.ts
@@ -2,7 +2,8 @@ import * as fs from 'fs';
 import * as assert from 'assert';
 import { ObjectID } from 'bson';
 import { randomBytes } from 'crypto';
-import { ObjectEncoder, CustomTypeProcessor, ObjectDecoder } from '../lib';
+import { test } from 'sarg';
+import { ObjectEncoder, CustomTypeProcessor, ObjectDecoder } from '../browser';
 
 class ProcessorObjectID extends CustomTypeProcessor<ObjectID> {
     validate(id: any): boolean {
@@ -16,142 +17,139 @@ class ProcessorObjectID extends CustomTypeProcessor<ObjectID> {
     }
 }
 
-export default function() {
-    return {
-        'it should support date field objects': function() {
-        	let buffer: Buffer;
-        	const sourceObject = {
-        		user: {
-        			name: 'Danny Rayburn',
-        			createdAt: new Date('2018-04-17T21:36:54.033Z'),
-        		}
-        	};
 
-        	buffer = new ObjectEncoder().encode(sourceObject);
-
-            const decoder = new ObjectDecoder(buffer);
-
-            assert.deepEqual(decoder.decode(), sourceObject);
-        },
-
-        'it should throw when tries to access beyond buffer length': function() {
-            const buffer = new ObjectEncoder().encode({
-                id: 10
-            });
-            const decoder = new ObjectDecoder(buffer);
-
-            assert.deepEqual(decoder.decode(), { id: 10 });
-            assert.throws(function() {
-                decoder.decode();
-            });
-        },
-
-        'it should encode / decode big and deep javascript object': function() {
-        	let buffer: Buffer;
-        	const sourceObject = JSON.parse(fs.readFileSync(__dirname + '/test.json', 'utf8'));
-
-        	buffer = new ObjectEncoder().encode(sourceObject);
-
-        	assert.deepEqual(new ObjectDecoder(buffer).decode(), sourceObject);
-        },
-
-        'it should encode / decode boolean fields': function() {
-        	assert.deepEqual(new ObjectDecoder(new ObjectEncoder().encode({
-        		done: true
-        	})).decode(), { done: true });
-        },
-
-        'it should support undefined values': function() {
-        	assert.deepEqual(new ObjectDecoder(new ObjectEncoder().encode({
-        		value: undefined
-        	})).decode(), { value: undefined });
-        },
-
-        'it should support null values': function() {
-        	assert.deepEqual(new ObjectDecoder(new ObjectEncoder().encode({
-        		value: null
-        	})).decode(), { value: null });
-        },
-
-        'it should support ES6 map objects': function() {
-            const map = new Map<any, Buffer>();
-
-            for(let i = 0; i < 100; i++) {
-                map.set(i, randomBytes(128));
-            }
-
-            map.set('(', Buffer.from(')', 'utf8'));
-            map.set(Buffer.from('key 1', 'utf8'), Buffer.from('value 1', 'utf8'));
-            map.set(Buffer.from('key 2', 'utf8'), Buffer.from('value 2', 'utf8'));
-
-            assert.deepEqual(new ObjectDecoder(new ObjectEncoder().encode({
-                mapsList: [map]
-            })).decode(), { mapsList: [map] });
-        },
-
-        'it should support all kinds of buffers': function() {
-            const source = {
-                nativeArrayBuffer: Buffer.from('simple text for array buffer', 'utf8'),
-                bytes: randomBytes(256)
-            };
-
-            const decoded = new ObjectDecoder(new ObjectEncoder().encode(source)).decode();
-
-            assert.ok(Buffer.from(source.nativeArrayBuffer).equals(Buffer.from(decoded.nativeArrayBuffer)));
-        },
-
-        'it should support maximum of uint32': function() {
-            const buffer = new ObjectEncoder().encode({
-                maxUint32: 4294967295
-            });
-            assert.deepEqual(new ObjectDecoder(buffer).decode(), {
-                maxUint32: 4294967295
-            });
-        },
-
-        'it should support minimum of int32': function() {
-            const buffer = new ObjectEncoder().encode({
-                maxInt32: -2147483648
-            });
-            assert.deepEqual(new ObjectDecoder(buffer).decode(), {
-                maxInt32: -2147483648
-            });
-        },
-
-        'it should support maximum of int32': function() {
-            const buffer = new ObjectEncoder().encode({
-                maxInt32: 2147483647
-            });
-            assert.deepEqual(new ObjectDecoder(buffer).decode(), {
-                maxInt32: 2147483647
-            });
-        },
-
-        'it should provide features for custom types': function() {
-            const user = { _id: new ObjectID() };
-            const instructions = [{
-                processor: new ProcessorObjectID,
-                value: 128
-            }];
-
-            const buffer = new ObjectEncoder(instructions).encode({
-                user
-            });
-
-            const decoded = new ObjectDecoder(buffer, instructions).decode();
-
-            assert.ok(decoded.user._id instanceof ObjectID);
-            assert.ok(decoded.user._id.equals(user._id));
-        },
-
-        'it should support special characters': function() {
-            assert.deepEqual(new ObjectDecoder(new ObjectEncoder().encode({ name: 'Cristóvão Galvão' })).decode(), {
-                name: 'Cristóvão Galvão'
-            });
-
-            assert.deepEqual(new ObjectDecoder(new ObjectEncoder().encode({ specialText: '¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖÙÚÛÜÝÞàáâãäåæçèéêëìíîïðñòóôõöùúûüýþÿ' })).decode(), {
-                specialText: '¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖÙÚÛÜÝÞàáâãäåæçèéêëìíîïðñòóôõöùúûüýþÿ'
-            });
+test('it should support date field objects', function() {
+    let buffer: Buffer;
+    const sourceObject = {
+        user: {
+            name: 'Danny Rayburn',
+            createdAt: new Date('2018-04-17T21:36:54.033Z'),
         }
     };
-}
+
+    buffer = new ObjectEncoder().encode(sourceObject);
+
+    const decoder = new ObjectDecoder(buffer);
+
+    assert.deepEqual(decoder.decode(), sourceObject);
+});
+
+test('it should throw when tries to access beyond buffer length', function() {
+    const buffer = new ObjectEncoder().encode({
+        id: 10
+    });
+    const decoder = new ObjectDecoder(buffer);
+
+    assert.deepEqual(decoder.decode(), { id: 10 });
+    assert.throws(function() {
+        decoder.decode();
+    });
+});
+
+test('it should encode / decode big and deep javascript object', function() {
+    let buffer: Buffer;
+    const sourceObject = JSON.parse(fs.readFileSync(__dirname + '/test.json', 'utf8'));
+
+    buffer = new ObjectEncoder().encode(sourceObject);
+
+    assert.deepEqual(new ObjectDecoder(buffer).decode(), sourceObject);
+});
+
+test('it should encode / decode boolean fields', function() {
+    assert.deepEqual(new ObjectDecoder(new ObjectEncoder().encode({
+        done: true
+    })).decode(), { done: true });
+});
+
+test('it should support undefined values', function() {
+    assert.deepEqual(new ObjectDecoder(new ObjectEncoder().encode({
+        value: undefined
+    })).decode(), { value: undefined });
+});
+
+test('it should support null values', function() {
+    assert.deepEqual(new ObjectDecoder(new ObjectEncoder().encode({
+        value: null
+    })).decode(), { value: null });
+});
+
+test('it should support ES6 map objects', function() {
+    const map = new Map<any, Buffer>();
+
+    for(let i = 0; i < 100; i++) {
+        map.set(i, randomBytes(128));
+    }
+
+    map.set('(', Buffer.from(')', 'utf8'));
+    map.set(Buffer.from('key 1', 'utf8'), Buffer.from('value 1', 'utf8'));
+    map.set(Buffer.from('key 2', 'utf8'), Buffer.from('value 2', 'utf8'));
+
+    assert.deepEqual(new ObjectDecoder(new ObjectEncoder().encode({
+        mapsList: [map]
+    })).decode(), { mapsList: [map] });
+});
+
+test('it should support all kinds of buffers', function() {
+    const source = {
+        nativeArrayBuffer: Buffer.from('simple text for array buffer', 'utf8'),
+        bytes: randomBytes(256)
+    };
+
+    const decoded = new ObjectDecoder(new ObjectEncoder().encode(source)).decode();
+
+    assert.ok(Buffer.from(source.nativeArrayBuffer).equals(Buffer.from(decoded.nativeArrayBuffer)));
+});
+
+test('it should support maximum of uint32', function() {
+    const buffer = new ObjectEncoder().encode({
+        maxUint32: 4294967295
+    });
+    assert.deepEqual(new ObjectDecoder(buffer).decode(), {
+        maxUint32: 4294967295
+    });
+});
+
+test('it should support minimum of int32', function() {
+    const buffer = new ObjectEncoder().encode({
+        maxInt32: -2147483648
+    });
+    assert.deepEqual(new ObjectDecoder(buffer).decode(), {
+        maxInt32: -2147483648
+    });
+});
+
+test('it should support maximum of int32', function() {
+    const buffer = new ObjectEncoder().encode({
+        maxInt32: 2147483647
+    });
+    assert.deepEqual(new ObjectDecoder(buffer).decode(), {
+        maxInt32: 2147483647
+    });
+});
+
+test('it should provide features for custom types', function() {
+    const user = { _id: new ObjectID() };
+    const instructions = [{
+        processor: new ProcessorObjectID,
+        value: 128
+    }];
+
+    const buffer = new ObjectEncoder(instructions).encode({
+        user
+    });
+
+    const decoded = new ObjectDecoder(buffer, instructions).decode();
+
+    assert.ok(decoded.user._id instanceof ObjectID);
+    assert.ok(decoded.user._id.equals(user._id));
+});
+
+test('it should support special characters', function() {
+    assert.deepEqual(new ObjectDecoder(new ObjectEncoder().encode({ name: 'Cristóvão Galvão' })).decode(), {
+        name: 'Cristóvão Galvão'
+    });
+
+    assert.deepEqual(new ObjectDecoder(new ObjectEncoder().encode({ specialText: '¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖÙÚÛÜÝÞàáâãäåæçèéêëìíîïðñòóôõöùúûüýþÿ' })).decode(), {
+        specialText: '¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖÙÚÛÜÝÞàáâãäåæçèéêëìíîïðñòóôõöùúûüýþÿ'
+    });
+});
--- test/test_bindings.ts
+++ test/test_bindings.ts
@@ -1,6 +1,8 @@
-import * as bo from '../';
 import * as assert from 'assert';
+import { test } from 'sarg';
 import { randomBytes } from 'crypto';
+
+const bo = require('../');
 
 class User {
     id: number; // uint32
@@ -43,79 +45,75 @@ class UserProcessor extends bo.CustomTypeProcessor<User> {
     }
 }
 
-export default function() {
-    return {
-        'it should encode complex object': function() {
-            const buffer = new bo.ObjectEncoder().encode(require('./test.json'));
-            assert.deepEqual(new bo.ObjectDecoder(buffer).decode(), require('./test.json'));
-        },
+test('it should encode complex object', function() {
+    const buffer = new bo.ObjectEncoder().encode(require('./test.json'));
+    assert.deepEqual(new bo.ObjectDecoder(buffer).decode(), require('./test.json'));
+});
 
-        'it should encode native boolean parameters': function() {
-            const buffer = new bo.ObjectEncoder().encode({ wellTested: true, badTested: false });
+test('it should encode native boolean parameters', function() {
+    const buffer = new bo.ObjectEncoder().encode({ wellTested: true, badTested: false });
 
-            assert.deepEqual(new bo.ObjectDecoder(buffer).decode(), { wellTested: true, badTested: false });
-        },
+    assert.deepEqual(new bo.ObjectDecoder(buffer).decode(), { wellTested: true, badTested: false });
+});
 
-        'it should encode undefined': function() {
-            assert.deepEqual(new bo.ObjectDecoder(new bo.ObjectEncoder().encode({ undefinedValue: undefined })).decode(), {
-                undefinedValue: undefined
-            });
-        },
+test('it should encode undefined', function() {
+    assert.deepEqual(new bo.ObjectDecoder(new bo.ObjectEncoder().encode({ undefinedValue: undefined })).decode(), {
+        undefinedValue: undefined
+    });
+});
 
-        'it should encode null value': function() {
-            assert.deepEqual(new bo.ObjectDecoder(new bo.ObjectEncoder().encode({ id: null })).decode(), { id: null });
-        },
+test('it should encode null value', function() {
+    assert.deepEqual(new bo.ObjectDecoder(new bo.ObjectEncoder().encode({ id: null })).decode(), { id: null });
+});
 
-        'it should encode buffer value': function() {   
-            const id = randomBytes(64);
-            const decoded = new bo.ObjectDecoder(new bo.ObjectEncoder().encode({
-                id
-            })).decode();
+test('it should encode buffer value', function() {   
+    const id = randomBytes(64);
+    const decoded = new bo.ObjectDecoder(new bo.ObjectEncoder().encode({
+        id
+    })).decode();
 
-            assert.deepEqual(decoded, { id });
-        },
+    assert.deepEqual(decoded, { id });
+});
 
-        'it should encode date objects': function() {
-            const date = new Date();
+test('it should encode date objects', function() {
+    const date = new Date();
 
-            assert.deepEqual(new bo.ObjectDecoder(new bo.ObjectEncoder().encode([{ createdAt: date }])).decode(), [{
-                createdAt: date
-            }]);
-        },
+    assert.deepEqual(new bo.ObjectDecoder(new bo.ObjectEncoder().encode([{ createdAt: date }])).decode(), [{
+        createdAt: date
+    }]);
+});
 
-        'it should throw when receive an invalid instructions': function() {
-            assert.throws(function() {
-                new bo.ObjectEncoder(<any>{});
-            });
-        },
+test('it should throw when receive an invalid instructions', function() {
+    assert.throws(function() {
+        new bo.ObjectEncoder(<any>{});
+    });
+});
 
-        'it should encode objects with custom types': function() {
-            const instructions = [{
-                value: 80,
-                processor: new UserProcessor
-            }];
-            const encoder = new bo.ObjectEncoder(instructions);
-            const buffer = encoder.encode({
-                users: [new User(1, 'victor'), new User(2, 'gallins')]
-            });
+test('it should encode objects with custom types', function() {
+    const instructions = [{
+        value: 80,
+        processor: new UserProcessor
+    }];
+    const encoder = new bo.ObjectEncoder(instructions);
+    const buffer = encoder.encode({
+        users: [new User(1, 'victor'), new User(2, 'gallins')]
+    });
 
-            const decoded: { users: any[] } = new bo.ObjectDecoder(buffer, instructions).decode();
+    const decoded: { users: any[] } = new bo.ObjectDecoder(buffer, instructions).decode();
 
-            decoded.users.map(user => assert.ok(user instanceof User, 'all results inside `users` property should be equal to `User` instance'));
+    decoded.users.map(user => assert.ok(user instanceof User, 'all results inside `users` property should be equal to `User` instance'));
 
-            assert.deepEqual(decoded, {
-                users: [new User(1, 'victor'), new User(2, 'gallins')]
-            });
-        },
+    assert.deepEqual(decoded, {
+        users: [new User(1, 'victor'), new User(2, 'gallins')]
+    });
+});
 
-        'it should support special characters': function() {
-            assert.deepEqual(new bo.ObjectDecoder(new bo.ObjectEncoder().encode({ name: 'Cristóvão Galvão' })).decode(), {
-                name: 'Cristóvão Galvão'
-            });
+test('it should support special characters', function() {
+    assert.deepEqual(new bo.ObjectDecoder(new bo.ObjectEncoder().encode({ name: 'Cristóvão Galvão' })).decode(), {
+        name: 'Cristóvão Galvão'
+    });
 
-            assert.deepEqual(new bo.ObjectDecoder(new bo.ObjectEncoder().encode({ specialText: '¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖÙÚÛÜÝÞàáâãäåæçèéêëìíîïðñòóôõöùúûüýþÿ' })).decode(), {
-                specialText: '¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖÙÚÛÜÝÞàáâãäåæçèéêëìíîïðñòóôõöùúûüýþÿ'
-            });
-        }
-    };
-}
+    assert.deepEqual(new bo.ObjectDecoder(new bo.ObjectEncoder().encode({ specialText: '¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖÙÚÛÜÝÞàáâãäåæçèéêëìíîïðñòóôõöùúûüýþÿ' })).decode(), {
+        specialText: '¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖÙÚÛÜÝÞàáâãäåæçèéêëìíîïðñòóôõöùúûüýþÿ'
+    });
+});
