3bfce24ef13b0c1110946a5d1de81a1f5394bb5d
Victor Queiroz <victorcqueirozg@gmail.com>
2019-04-08 01:44:44 -0300

    src/custom-type.h: add comment to avoid accidental memory leaks
    Make explicit that `input_buffer` argument needs to be manually allocated and that it's ownership will be forwarded to the Node.js buffer created by CustomType::Decode function.

--- src/custom-type.h
+++ src/custom-type.h
@@ -9,6 +9,10 @@ using namespace v8;
 namespace CustomType {
     uint8_t Validate(Local<Object> processor, Local<Value> value);
     uint8_t Encode(Local<Object> processor, Local<Value> value, uint8_t** result, size_t* byte_length);
+    /**
+     * Decode custom type using a function available in processor. Attention to `input_buffer` argument, it
+     * should be previously allocated so this function can take ownership of it's deallocation
+     */
     Local<Value> Decode(size_t byte_length, uint8_t* input_buffer, Local<Object> processor);
 }
 
